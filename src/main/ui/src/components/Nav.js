564cafcf82a5d23d991dd10c067a659a419873ca
Rose <rosevandergiessen@proton.me>
2023-07-13 10:52:07 +0200

    Add profile picture to navbar

--- src/main/ui/src/components/Nav.js
+++ src/main/ui/src/components/Nav.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import AuthService from '../services/AuthService';
 import '../styles/Nav.css';
 
 export const NavBar = () => {
     const currentUser = AuthService.getCurrentUser();
+    const [user, setUser] = useState(false);
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    const fetchUser = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/users/${currentUser.id}`);
+            const data = await response.json();
+            setUser(data);
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching user:', error);
+        }
+    }
 
     const handleLogout = () => {
         AuthService.logout();
@@ -21,6 +37,13 @@ export const NavBar = () => {
                     <NavLink to="/accepted-requests">ACCEPTED REQUESTS</NavLink>
                 </div>
                 <div className="navbar-right">
+                    {user.imageData && (
+                        <img
+                            className="nav-bar-image"
+                            src={`data:image/*;base64,${user.imageData}`}
+                            alt="User photo"
+                        />
+                    )}
                     {currentUser ? (
                         <div className="user-info">
                             <Link className="username" to="/my-profile">{currentUser.username.toUpperCase()}</Link>
